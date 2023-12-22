0ad50914a040ec0a9f800ecca6044f935c1597de
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-12-22 17:44:22 +0200

    display both small and large list items

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import LargeAuthorListItem from "./components/authors/LargeListItem";
 import SmallAuthorListItem from "./components/authors/SmallListItem";
 import RegularList from "./components/lists/RegularList";
 import { authors } from "./data/authors";
@@ -8,6 +9,12 @@ function App() {
       <RegularList
         items={authors}
         ItemComponent={SmallAuthorListItem}
+        sourceName="author"
+      />
+      <hr />
+      <RegularList
+        items={authors}
+        ItemComponent={LargeAuthorListItem}
         sourceName="author"
       />
     </>
