ce2fe703e02c4b45dc2f9968ad253ee435020a8f
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-12-22 18:32:04 +0200

    add Modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,18 @@
 // import LargeAuthorListItem from "./components/authors/LargeListItem";
 // import SmallAuthorListItem from "./components/authors/SmallListItem";
+import Modal from "./components/Modal";
 import LargeBookListItem from "./components/books/LargeListItem";
-import SmallBookListItem from "./components/books/SmallListItem";
-import RegularList from "./components/lists/RegularList";
+// import SmallBookListItem from "./components/books/SmallListItem";
+// import RegularList from "./components/lists/RegularList";
 // import { authors } from "./data/authors";
 import { books } from "./data/books";
 
 function App() {
   return (
     <>
-      {/* <RegularList
-        items={authors}
-        ItemComponent={SmallAuthorListItem}
-        sourceName="author"
-      />
-      <hr />
-      <RegularList
-        items={authors}
-        ItemComponent={LargeAuthorListItem}
-        sourceName="author"
-      /> */}
-      <RegularList
-        items={books}
-        ItemComponent={SmallBookListItem}
-        sourceName="book"
-      />
-      <hr />
-      <RegularList
-        items={books}
-        ItemComponent={LargeBookListItem}
-        sourceName="book"
-      />
+      <Modal>
+        <LargeBookListItem book={books[0]} />
+      </Modal>
     </>
   );
 }
---
+++ src/components/Modal.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { styled } from "styled-components";
+
+const ModalOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #00000075;
+`;
+
+const ModalContent = styled.div`
+  margin: 12% auto;
+  width: 50%;
+  padding: 25px;
+  background-color: white;
+  color: black;
+`;
+const Modal = ({ children }: { children: React.ReactNode }) => {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button onClick={() => setShow(true)}>Show Modal</button>
+      {show && (
+        <ModalOverlay onClick={() => setShow(false)}>
+          <ModalContent onClick={(e: any) => e.stopPropagation()}>
+            <button onClick={() => setShow(false)}>Close Modal</button>
+            <hr />
+            {children}
+          </ModalContent>
+        </ModalOverlay>
+      )}
+    </>
+  );
+};
+
+export default Modal;
