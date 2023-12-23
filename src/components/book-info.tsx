a487fed0786a280f9190a392da91a13b557837c6
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-12-23 04:07:07 +0200

    add user-info,book-info components and server.js file

---
+++ src/components/book-info.tsx
@@ -0,0 +1,16 @@
+import { BooksType } from "../data/books";
+
+export const BookInfo = ({ book }: { book: BooksType }) => {
+  const { name, price, title, pages } = book || {};
+
+  return book ? (
+    <>
+      <h3>{name}</h3>
+      <p>{price}</p>
+      <h3>Title: {title}</h3>
+      <p>Number of Pages: {pages}</p>
+    </>
+  ) : (
+    <h1>Loading</h1>
+  );
+};
---
+++ src/components/user-info.tsx
@@ -0,0 +1,20 @@
+import { AuthorsType } from "../data/authors";
+
+export const UserInfo = ({ user }: { user: AuthorsType }) => {
+  const { name, age, country, books } = user || {};
+  return user ? (
+    <>
+      <h2>{name}</h2>
+      <p>Age: {age} years</p>
+      <p>Country: {country}</p>
+      <h2>Books</h2>
+      <ul>
+        {books.map((book) => (
+          <li key={book}> {book} </li>
+        ))}
+      </ul>
+    </>
+  ) : (
+    <h1>Loading...</h1>
+  );
+};
