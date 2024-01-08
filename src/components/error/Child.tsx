76e1a061c4eee4a1bf64bcd7cab9510eb1517af0
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-01-08 20:01:58 +0200

    trying event listeners on capturing and bubbling phase

--- src/components/error/Child.tsx
+++ src/components/error/Child.tsx
@@ -1,6 +1,14 @@
 const Child = () => {
   //   throw new Error("error occured in child");
-  return <div style={{ margin: "40px 0 0 0" }}>iam child component</div>;
+  return (
+    <div
+      style={{ margin: "40px 0 0 0" }}
+      // onClick={() => console.log("inner child")}
+      onClickCapture={() => console.log("inner child")}
+    >
+      iam child component
+    </div>
+  );
 };
 
 export default Child;
--- src/components/error/Parent.tsx
+++ src/components/error/Parent.tsx
@@ -3,7 +3,11 @@ import Child from "./Child";
 
 const Parent = () => {
   return (
-    <div style={{ width: "50%", fontSize: "35px", margin: "40px auto" }}>
+    <div
+      style={{ width: "50%", fontSize: "35px", margin: "40px auto" }}
+      // onClick={() => console.log("outer parent")}
+      onClickCapture={() => console.log("outer parent")}
+    >
       iam parent
       <ErrorBoundry fallback={<h1>Error at child level</h1>}>
         <Child />
