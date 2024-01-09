2ec4ca1668e6617ce2feb6d295662922ac0ee0dd
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-01-09 13:08:21 +0200

    using useCallback as a ref

--- src/components/CallbackAsRef.tsx
+++ src/components/CallbackAsRef.tsx
@@ -1,14 +1,18 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useState } from "react";
 
 const CallbackAsRef = () => {
   const [show, setShow] = useState(false);
-  const inputRef = useRef<HTMLInputElement>(null);
+  //   const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useCallback((input: HTMLInputElement) => {
+    if (input === null) return;
+    input.focus();
+  }, []);
 
-  useEffect(() => {
-    if (inputRef?.current) {
-      inputRef?.current?.focus();
-    }
-  }, [show]);
+  //   useEffect(() => {
+  //     if (inputRef?.current) {
+  //       inputRef?.current?.focus();
+  //     }
+  //   }, [show]);
   return (
     <div>
       <button onClick={() => setShow((prev: boolean) => !prev)}>
