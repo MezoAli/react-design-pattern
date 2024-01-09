99da7d28a4948a92b157bf080923f6975f03ea40
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-01-09 12:50:47 +0200

    using useLayoutEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import "./App.css";
-import Parent from "./components/error/Parent";
 function App() {
+  const [show, setShow] = useState(false);
+  const [top, setTop] = useState(0);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  // useLayoutEffect(() => {
+  useEffect(() => {
+    if (buttonRef.current === null || !show) {
+      return setTop(0);
+    }
+    const obj = buttonRef?.current?.getBoundingClientRect();
+    setTop(obj.bottom + 30);
+  }, [show]);
+
+  const now = performance.now();
+  while (now > performance.now() - 100) {}
   return (
     <>
-      <Parent />
+      <button ref={buttonRef} onClick={() => setShow((i) => !i)}>
+        show
+      </button>
+      {show && (
+        <div
+          style={{
+            top: `${top}px`,
+            backgroundColor: "red",
+            padding: "20px",
+            position: "absolute",
+            width: "100%",
+            display: "block",
+          }}
+        >
+          some text
+        </div>
+      )}
     </>
   );
 }
